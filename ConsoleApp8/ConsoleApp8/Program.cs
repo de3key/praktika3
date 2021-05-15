57b030f1c7575be7d32e8f93ccc681ded3252464
de3key <gnosthard@gmail.com>
2021-05-15 17:06:08 +0700

    praktika

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введи сюда свой z");
+            double z = Convert.ToDouble(Console.ReadLine());
+            double a = 0;
+            if (z > 0)
+            {
+                a = 1;
+            }
+            if (z == 0)
+            {
+                a = 0;
+            }
+            if (z < 0)
+            {
+                a = -1;
+            }
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int a = x / 100;
+            int b = x % 100 / 10;
+            int c = x % 10;
+            int g = a + b + c;
+            if (g % 5 == 0)
+            {
+                Console.WriteLine("Да");
+            }
+            else
+            {
+                Console.WriteLine("Нет");
+            }
+            Console.ReadKey();
+        }
+    }
+}
